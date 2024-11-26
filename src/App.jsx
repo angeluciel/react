d536cd10d7de5b854162eba1574ad05f1e9dabc5
Luciel <jpmizidoro@gmail.com>
2024-11-26 17:50:32 -0300

    NavBar
    created the navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import Hero from "./components/Hero.jsx";
 import About from "./components/About.jsx";
+import NavBar from "./components/NavBar.jsx";
 
 const App = () => {
   return (
     <main className="relative min-h-screen w-screen overflow-x-hidden">
+      <NavBar />
       <Hero />
-
       <About />
     </main>
   );
